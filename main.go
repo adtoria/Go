9f71e32882e556cbb34f6f77a614e50fc377e750
adtoria <adityyyyadav@gmail.com>
2023-10-19 11:02:28 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+// formatting strings and printing messages
+import "fmt"
+
+// main() is the entry point of the application
+func main() {
+	age := 35
+	name := "shaun"
+
+	// Printf (formatted strings) %_ = format specifier
+	fmt.Printf("my age is %v and my name is %v \n", age, name)
+	// %v - default format specifier for variables
+	fmt.Printf("my age is %v and my name is %q \n", age, name)
+	// %q - puts double quotes around strings
+
+	fmt.Printf("age is of type %T \n", age)
+
+	fmt.Printf("you scored %0.2f points! \n", 225.55)
+
+	// Sprintf (save formatted strings)
+	var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
+	fmt.Println("the saved string is:", str)
+}
